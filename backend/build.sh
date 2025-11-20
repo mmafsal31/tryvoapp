aa44ad9c4cc3594bb68966f2daecb7aa631ac956
mmafsal31 <mmafsal31@gmail.com>
2025-11-20 12:17:37 +0530

    Fix build.sh path to requirements.txt

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Move to backend folder
+cd backend
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Run migrations
 python manage.py migrate
+
+# Collect static files
 python manage.py collectstatic --noinput
