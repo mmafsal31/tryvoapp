b1776c500e48fc1370dad6ea17606c5b25bf6d43
mmafsal31 <mmafsal31@gmail.com>
2025-11-22 01:10:10 +0530

    Fix build.sh permissions 12345678

--- backend/settings.py
+++ backend/settings.py
@@ -135,11 +135,15 @@ MEDIA_ROOT = BASE_DIR / "media"
 # CORS
 # ----------------------------
 CORS_ALLOWED_ORIGINS = [
-    "https://tryvo.netlify.app/",
+    "https://tryvo.netlify.app",
 ]
-
+CORS_ALLOWED_ORIGINS += [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+]
 CSRF_TRUSTED_ORIGINS = [
-    "https://tryvo.netlify.app/",
+    "https://tryvo.netlify.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
